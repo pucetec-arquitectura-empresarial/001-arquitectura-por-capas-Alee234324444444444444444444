f90a5f6d9e1fda1b42b74ff40708b2e600337e27
Alejandro <apbarrionuevo@puce.edu.ec>
2025-05-25 12:27:25 -0500

    NOMBRAR COLUMNAS

--- src/main/kotlin/com/puce/achievements/models/entities/Achievement.kt
+++ src/main/kotlin/com/puce/achievements/models/entities/Achievement.kt
@@ -12,8 +12,11 @@ data class Achievement(
     @Id @GeneratedValue(strategy = IDENTITY)
     val id: Long = 0,
     val title: String = "",
+    @Column(name = "description_at")
     val description: String = "",
+    @Column(name = "category_at")
     val category: String = "",
+    @Column(name = "date_at")
     val date: LocalDate = LocalDate.now(),
     @Column(name = "student_name")
     val studentName: String = ""
